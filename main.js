860065618150e4f7130b6c3571777c70b8dca8a4
Kacper Adamczyk <kacperadam10@gmail.com>
2022-07-10 15:12:24 +0200

    basic excercise

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'red dead redemption II',
+            description: 'this is an awesome game that was created by Rockstar games'
+        }
+    }
+})  
